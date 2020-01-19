84d06f37c8cce0817bd922fe2e27bf4e5dde4982
Claudiu <cduneata@gmail.com>
2020-01-19 19:37:54 +0200

    Add solution to FizzBuzz

--- fizzbuzz/fizbuzz.py
+++ fizzbuzz/fizbuzz.py
@@ -5,3 +5,15 @@ Write a program that prints the numbers from 1 to 100. But:
  - For numbers which are multiples of both three and five print “FizzBuzz”.
 """
 
+
+def fizz_buzz(number):
+    for x in range(number):
+        if x % 3 == 0:
+            print("Fizz", x)
+        if x % 5 == 0:
+            print("Buzz", x)
+        else:
+            print("FizzBuzz")
+
+
+fizz_buzz(101)
